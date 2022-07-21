33616661754cb6abef9fe3414b1c3e35af871eea
Will <willchesson@gmail.com>
2022-07-21 13:33:59 -0400

    commit tree 75eae0ce74019417589d8bf935c1998ab8f143b7

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require "pry"
 
 class LinkedList
   attr_accessor :head, :tail
@@ -9,6 +10,38 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if head.nil?
+
+    length = 0
+    node = head
+
+    until node.nil?
+      node = node.next_node
+      length += 1
+    end
+
+    if length == 1
+      return
+    end
+
+    head_index = 0
+    tail_index = length - 1
+
+
+    if length > 3
+      until head_index == tail_index
+      head&.next_node = tail&.prev_node
+      head.next_node = head.next_node.next_node
+      tail.prev_node = tail.prev_node.prev_node
+      head_index += 1
+      tail_index -= 1
+      
+      end
+    else
+      # binding.pry
+    head = tail
+    tail = head
+    end
+    
   end
 end
